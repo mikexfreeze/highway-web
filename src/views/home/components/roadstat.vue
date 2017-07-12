164e5475dd3ab7580eccdabdd763b96bb6b6d1f1
杨胜富 <ysfnevercry@gmail.com>
2017-07-12 23:34:23 +0800

    状态调整

--- src/views/home/components/roadstat.vue
+++ src/views/home/components/roadstat.vue
@@ -2,12 +2,12 @@
     <div class="road_stat_unit">
         <el-row>
             <el-col :span="2">
-                <div class="sensor" :class="{ 'green': roadstatus.s8 == 0, 'red': roadstatus.s8 == 1 , 'blue': roadstatus.s8 == 2 }">
+                <div class="sensor" :class="{ 'green': roadstatus.s8 == 0, 'red': roadstatus.s8 == 2 , 'blue': roadstatus.s8 == 1 }">
                     <span>9</span>
                 </div>
             </el-col>
             <el-col :span="2" :offset="18">
-                <div class="sensor" :class="{ 'green': roadstatus.s9 == 0, 'red': roadstatus.s9 == 1, 'blue': roadstatus.s9 == 2 }">
+                <div class="sensor" :class="{ 'green': roadstatus.s9 == 0, 'red': roadstatus.s9 == 2, 'blue': roadstatus.s9 == 1 }">
                     <span>10</span>
                 </div>
             </el-col>
@@ -19,12 +19,12 @@
         </el-row>
         <el-row>
             <el-col :span="2">
-                <div class="sensor" :class="{ 'green': roadstatus.s6 == 0, 'red': roadstatus.s6 == 1 , 'blue': roadstatus.s6 == 2}">
+                <div class="sensor" :class="{ 'green': roadstatus.s6 == 0, 'red': roadstatus.s6 == 2 , 'blue': roadstatus.s6 == 1}">
                     <span>7</span>
                 </div>
             </el-col>
             <el-col :span="2" :offset="18">
-                <div class="sensor" :class="{ 'green': roadstatus.s7 == 0, 'red': roadstatus.s7 == 1 , 'blue': roadstatus.s7 == 2}">
+                <div class="sensor" :class="{ 'green': roadstatus.s7 == 0, 'red': roadstatus.s7 == 2 , 'blue': roadstatus.s7 == 1}">
                     <span>8</span>
                 </div>
             </el-col>
@@ -35,12 +35,12 @@
         </el-row>
         <el-row>
             <el-col :span="2">
-                <div class="sensor" :class="{ 'green': roadstatus.s4 == 0, 'red': roadstatus.s4 == 1, 'blue': roadstatus.s4 == 2 }">
+                <div class="sensor" :class="{ 'green': roadstatus.s4 == 0, 'red': roadstatus.s4 == 2, 'blue': roadstatus.s4 == 1 }">
                     <span>5</span>
                 </div>
             </el-col>
             <el-col :span="2" :offset="18">
-                <div class="sensor" :class="{ 'green': roadstatus.s5 == 0, 'red': roadstatus.s5 == 1, 'blue': roadstatus.s5 == 2 }">
+                <div class="sensor" :class="{ 'green': roadstatus.s5 == 0, 'red': roadstatus.s5 == 2, 'blue': roadstatus.s5 == 1 }">
                     <span>6</span>
                 </div>
             </el-col>
@@ -51,12 +51,12 @@
         </el-row>
         <el-row>
             <el-col :span="2">
-                <div class="sensor" :class="{ 'green': roadstatus.s2 == 0, 'red': roadstatus.s2 == 1, 'blue': roadstatus.s2 == 2 }">
+                <div class="sensor" :class="{ 'green': roadstatus.s2 == 0, 'red': roadstatus.s2 == 2, 'blue': roadstatus.s2 == 1 }">
                     <span>3</span>
                 </div>
             </el-col>
             <el-col :span="2" :offset="18">
-                <div class="sensor" :class="{ 'green': roadstatus.s3 == 0, 'red': roadstatus.s3 == 1, 'blue': roadstatus.s3 == 2 }">
+                <div class="sensor" :class="{ 'green': roadstatus.s3 == 0, 'red': roadstatus.s3 == 2, 'blue': roadstatus.s3 == 1 }">
                     <span>4</span>
                 </div>
             </el-col>
@@ -77,12 +77,12 @@
         </el-row>
         <el-row>
             <el-col :span="2">
-                <div class="sensor" :class="{ 'green': roadstatus.s0 == 0, 'red': roadstatus.s0 == 1, 'blue': roadstatus.s0 == 2 }">
+                <div class="sensor" :class="{ 'green': roadstatus.s0 == 0, 'red': roadstatus.s0 == 2, 'blue': roadstatus.s0 == 1 }">
                     <span>1</span>
                 </div>
             </el-col>
             <el-col :span="2" :offset="18">
-                <div class="sensor" :class="{ 'green': roadstatus.s1 == 0, 'red': roadstatus.s1 == 1, 'blue': roadstatus.s1 == 2 }">
+                <div class="sensor" :class="{ 'green': roadstatus.s1 == 0, 'red': roadstatus.s1 == 2, 'blue': roadstatus.s1 == 1 }">
                     <span>2</span>
                 </div>
             </el-col>
--- src/views/home/components/status.vue
+++ src/views/home/components/status.vue
@@ -1,8 +1,8 @@
 <template>
     <span>
         <el-tag v-if="status == 0" type="success">正常状态</el-tag>
-        <el-tag v-if="status == 1" type="primary">检修预警</el-tag>
-        <el-tag v-if="status == 2" type="danger">维护预警</el-tag>
+        <el-tag v-if="status == 1" type="primary">传感器异常</el-tag>
+        <el-tag v-if="status == 2" type="danger">无传感器</el-tag>
     </span>
 </template>
 <script>
