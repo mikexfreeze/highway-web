58d360a4eaa5b36aa9980382181e01ac81064d69
杨胜富 <ysfnevercry@gmail.com>
2017-07-14 01:42:43 +0800

    更新地图

---
+++ src/views/map/components/dotview.vue
@@ -0,0 +1,70 @@
+
+
+<template>
+    <span class="dot">
+        <div class="dotview greenview" v-if="mapDot.stat == 0" ></div>
+        <div class="dotview blueview" v-if="mapDot.stat == 1" ></div>
+        <div class="dotview redview" v-if="mapDot.stat == 2" ></div>
+    </span>
+</template>
+<script>
+    export default{
+        props:['mapDot'],
+        data: function () {
+            return {
+                tmpDot: this.mapDot
+            }
+        },
+    }
+</script>
+
+<style scoped>
+    @-webkit-keyframes twinkling{	/*透明度由0到1*/
+        0%{
+            opacity:0.5;				/*透明度为0*/
+        }
+
+        40%{
+            opacity: 1;
+        }
+
+        80% {
+           opacity: 0.5;
+        }
+
+        100%{
+            opacity:0.3;				/*透明度为1*/
+        }
+    }
+
+
+    .dot {
+        position: absolute;
+    }
+
+    .dotview {
+        width: 30px;
+        height: 30px;
+        position: absolute;
+        -webkit-animation: twinkling 1.6s infinite ease-in-out;
+    }
+    
+    .greenview {
+        background-image: url('../../../assets/images/greendot.png');
+        background-size:cover
+    }
+    
+    .blueview {
+        background-image: url("../../../assets/images/bluedot.png");
+        background-size:cover
+    }
+    
+    .redview {
+        background-image: url("../../../assets/images/reddot.png");
+        background-size:cover
+    }
+    
+
+
+</style>
+
--- src/views/map/map.js
+++ src/views/map/map.js
@@ -3,121 +3,75 @@
  */
 import {GetXY} from './api/map'
 import axios from 'axios'
+import dotview from './components/dotview.vue'
 
 var randomState = 0;
 
 export default {
+    components:{dotview},
     data(){
-        return{}
+        return{
+            dotList: []
+        }
     },
     methods:{
         getXY(){
-            testXY();
-            // axios.get('http://192.168.1.203:8080/gaosuWeb/doMapShow')
-            //     .then(function (response) {
-            //         console.log(response);
-            //
-            //         var result = response.data;
-            //
-            //         $('#station_point').empty();
-            //
-            //         var station_pointE = $('#station_point');
-            //
-            //         for (var i = 0; i < result.length; i++) {
-            //             var stationStatus = result[i];
-            //
-            //             var stationStatusID = "station_status_" + i;
-            //
-            //             var pointHtml = "<div id='" + stationStatusID + "'></div>"
-            //
-            //             station_pointE.append(pointHtml);
-            //
-            //             var jQID = '#' + stationStatusID;
-            //
-            //             var statusColor;
-            //             var statusImage;
-            //
-            //             if (stationStatus["stat"] == 0) {
-            //                 statusColor = "green";
-            //                 statusImage = "url(../../assets/images/green_alert.bmp)";
-            //             } else if (stationStatus["stat"] == 1) {
-            //                 statusColor = "blue";
-            //                 statusImage = "url(../../assets/images/blue_alert.BMP)";
-            //             } else if (stationStatus["stat"] == 2) {
-            //                 statusColor = "red";
-            //                 statusImage = "url(../../assets/images/red_alert.bmp)";
-            //             }
-            //
-            //             var topValue = stationStatus["top"] + "px";
-            //             var leftValue = stationStatus["left"] + "px";
-            //
-            //             var cssParam = {
-            //                 "width": "30px",
-            //                 "height": "30px",
-            //                 // "background-color": statusColor,
-            //                 "position": "absolute",
-            //                 "top": topValue,
-            //                 "left": leftValue,
-            //                 "background-image": statusImage
-            //             }
-            //
-            //             $(jQID).css(cssParam);
-            //         }
-            //     })
-            //     .catch(function (error) {
-            //         console.log(error);
-            //     });
+            this.dotList = testXY();
+
+
+
         }
     }
 }
 
 function testXY() {
     var result = mapTestState();
+    return result;
 
-    $('#station_point').empty();
-
-    var station_pointE = $('#station_point');
-
-    for (var i = 0; i < result.length; i++) {
-        var stationStatus = result[i];
-
-        var stationStatusID = "station_status_" + i;
-
-        var pointHtml = "<div id='" + stationStatusID + "'></div>"
-
-        station_pointE.append(pointHtml);
-
-        var jQID = '#' + stationStatusID;
-
-        var statusColor;
-        var statusImage;
-
-        if (stationStatus["stat"] == 0) {
-            statusColor = "green";
-            // statusImage = "url(../../assets/images/green_alert.bmp)";
-        } else if (stationStatus["stat"] == 1) {
-            statusColor = "blue";
-            // statusImage = "url(../../assets/images/blue_alert.BMP)";
-        } else if (stationStatus["stat"] == 2) {
-            statusColor = "red";
-            // statusImage = "url(../../assets/images/red_alert.bmp)";
-        }
-
-        var topValue = stationStatus["top"] + "px";
-        var leftValue = stationStatus["left"] + "px";
-
-        var cssParam = {
-            "width": "30px",
-            "height": "30px",
-            // "background-color": statusColor,
-            "position": "absolute",
-            "top": topValue,
-            "left": leftValue,
-            "background-image": statusImage
-        }
-
-        $(jQID).css(cssParam);
-    }
+    // $('#station_point').empty();
+    //
+    // var station_pointE = $('#station_point');
+    //
+    // for (var i = 0; i < result.length; i++) {
+    //     var stationStatus = result[i];
+    //
+    //     var stationStatusID = "station_status_" + i;
+    //
+    //     var pointHtml = "<div id='" + stationStatusID + "'></div>"
+    //
+    //     station_pointE.append(pointHtml);
+    //
+    //     var jQID = '#' + stationStatusID;
+    //
+    //     var statusColor;
+    //     var statusImage;
+    //
+    //     if (stationStatus["stat"] == 0) {
+    //         statusColor = "green";
+    //         statusImage = "url('../../assets/images/greendot.png')";
+    //     } else if (stationStatus["stat"] == 1) {
+    //         statusColor = "blue";
+    //         statusImage = "url('../../assets/images/bluedot.png')";
+    //     } else if (stationStatus["stat"] == 2) {
+    //         statusColor = "red";
+    //         statusImage = "url('../../assets/images/reddot.png')";
+    //     }
+    //
+    //     var topValue = stationStatus["top"] + "px";
+    //     var leftValue = stationStatus["left"] + "px";
+    //
+    //     var cssParam = {
+    //         "width": "30px",
+    //         "height": "30px",
+    //         // "background-color": statusColor,
+    //         "position": "absolute",
+    //         "top": topValue,
+    //         "left": leftValue,
+    //         "background-image": statusImage
+    //     }
+    //
+    //     $(jQID).css(cssParam);
+    // }
 }
 
 function mapTestState() {
