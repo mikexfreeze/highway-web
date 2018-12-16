5a9c515fd8b9f360a0696e13d929ec518ede27ac
yang <ysfnevercry@gmail.com>
2018-12-17 01:58:54 +0800

    update

--- src/views/map/map.js
+++ src/views/map/map.js
@@ -6,13 +6,28 @@ import dotview from './components/dotview.vue'
 
 var randomState = 0;
 var timeSet;
+
+import ECharts from 'vue-echarts/components/ECharts.vue'
+require('echarts/lib/chart/bar');
+require('echarts/lib/chart/line');
+require('echarts/lib/chart/bar');
+require('echarts/lib/chart/pie');
+require('echarts/lib/component/tooltip');
+require('echarts/lib/component/toolbox');
+require('echarts/lib/component/legend');
+require('echarts/lib/component/markLine');
+require('echarts/lib/component/dataZoom');
+
+
 export default {
-    components:{dotview},
+    components:{dotview,chart: ECharts,},
     data(){
         return{
             dotList: [],
             imageWidth: 1059,
             imageHeight: 672,
+            show: true,
+            weightInfo:[{value:1000, name:"2轴车"},{value:1000, name:"3轴车"},{value:1000, name:"4轴车"},{value:1000, name:"5轴车"},{value:1000, name:"6轴车"}]
         }
     },
     mounted() {
@@ -64,6 +79,9 @@ export default {
         },
         getImageHeight(event) {
             this.imageHeight = document.getElementById('map-image').clientHeight;
+        },
+        hideWeightInfo() {
+
         }
     },
     created: function () {
@@ -75,6 +93,51 @@ export default {
             })
 
         }, 60000)
+
+
+        this.chart =  {
+            title: {
+                text: '天气情况统计',
+                subtext: '虚构数据',
+                left: 'center'
+            },
+            tooltip : {
+                trigger: 'item',
+                formatter: "{a} <br/>{b} : {c} ({d}%)"
+            },
+            legend: {
+                // orient: 'vertical',
+                // top: 'middle',
+                bottom: 10,
+                left: 'center',
+                data: ['2轴车', '3轴车','4轴车','5轴车','6轴车']
+            },
+            series : [
+                {
+                    type: 'pie',
+                    radius : '65%',
+                    center: ['50%', '50%'],
+                    selectedMode: 'single',
+                    data:[
+                        {value:535, name: '2轴车'},
+                        {value:510, name: '3轴车'},
+                        {value:634, name: '4轴车'},
+                        {value:735, name: '5轴车'},
+                        {value:735, name: '6轴车'}
+                    ],
+                    itemStyle: {
+                        emphasis: {
+                            shadowBlur: 10,
+                            shadowOffsetX: 0,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
+                    }
+                }
+            ]
+        }
+
+        let c1 = this.$refs.c1;
+        c1.mergeOptions(this.chart);
 
 
     },
--- src/views/map/map.vue
+++ src/views/map/map.vue
@@ -1,18 +1,31 @@
 <template>
     <div>
 
+        <div style="position: relative">
+            <img id="map-image"  src="../../assets/images/map.jpg" >
+            <div id='station_point' >
+                <dotview  v-for="(dot, index) in dotList" :key="index" :mapDot="dot" :style="{top: (dot.top * (imageHeight/545)) + 'px', left: (dot.left * (imageWidth/906)) + 'px'}"></dotview>
+            </div>
 
-        <img id="map-image"  src="../../assets/images/map.jpg" >
-        <div id='station_point' >
-            <dotview  v-for="(dot, index) in dotList" :key="index" :mapDot="dot" :style="{top: (dot.top * (imageHeight/545)) + 'px', left: (dot.left * (imageWidth/906)) + 'px'}"></dotview>
+
+            <transition name="fade">
+                <div class="weightInfo" v-if="show">
+                    <el-button  icon="arrow-right" @click="show = !show"></el-button>
+                    <h3 style="display: inline">统计信息</h3>
+                    <figure id="weightinfo-charts-contianer">
+                        <chart :options="chart"  ref="c1" auto-resize></chart>
+                    </figure>
+
+                </div>
+                <el-button class="weightInfoBtn" v-if="!show" @click="show =!show" >统<br/>计<br/>信<br/>息 </el-button>
+            </transition>
+
+
         </div>
-
-
-
-
-
     </div>
+
+
 </template>
 
 <script src="./map.js"></script>
-<style scoped src="./map.scss"></style>
+<style src="./map.scss"></style>
